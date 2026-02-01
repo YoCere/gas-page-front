6127112244c7ef3e8bbe17d685e6b18879d4766e
YoCere <jcerezorios@gmail.com>
2026-01-31 18:51:13 -0600

    fix a bug in routes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gas-page-front/"
+  base: "/gas-page-front/",
+  build: {
+    outDir: "docs"
+  }
 })

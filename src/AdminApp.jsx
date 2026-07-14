0c3207aea76de84295ac56890f034c85eddcfa1c
YoCere <jcerezorios@gmail.com>
2026-07-13 22:07:22 -0400

    new host

--- src/AdminApp.jsx
+++ src/AdminApp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { LogOut } from "lucide-react";
 
-const BACKEND_URL = "https://gas-page-back-production.up.railway.app";
+const BACKEND_URL = "http://vu084k0p7fh3tio5mdo9dljr.161.97.77.75.sslip.io";
 
 const AdminApp = ({ token, logout }) => {
   const [users, setUsers] = useState([]);
